f6017f6d306b430340ff9f1dd3afdd79eee72891
Hordur Heidarsson <heidarss@usc.edu>
2015-03-18 16:21:30 -0700

    Added 1ms delay to BESTUTM to make it arrive after BESTVEL

--- src/novatel_node.cpp
+++ src/novatel_node.cpp
@@ -513,8 +513,9 @@ public:
       ROS_INFO("Requesting default GPS messages: BESTUTMB, BESTVELB");
       std::stringstream default_logs;
       default_logs.precision(2);
+      // Receive position 1ms later than velocity to ensure they are received in that order
       default_logs << "BESPOSB ONTIME " << std::fixed << gps_default_logs_period_ << ";";
-      default_logs << "BESTUTMB ONTIME " << std::fixed << gps_default_logs_period_ << ";";
+      default_logs << "BESTUTMB ONTIME " << std::fixed << gps_default_logs_period_ << " " << 0.001 << ";";
       default_logs << "BESTVELB ONTIME " << std::fixed << gps_default_logs_period_;
       gps_.ConfigureLogs(default_logs.str());
     }
